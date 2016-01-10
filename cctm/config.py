58190dd179f47df1cac31387eeb7c69f5e1c2ef2
podhmo <podhmo+altair@beproud.jp>
2016-01-10 15:53:56 +0900

    help message when configuration file is missing

--- cctm/config.py
+++ cctm/config.py
@@ -1,11 +1,13 @@
 # -*- coding:utf-8 -*-
-from cctm import json
+import sys
 import os.path
 import logging
+from cctm import json
 from miniconfig_argparse import Configurator as ConfiguratorCore
 from miniconfig_argparse import ParserTreeControl as Control
 from cached_property import cached_property as reify
 from .path import pickup_file, safe_open
+from .exceptions import ConfigNotFound
 logger = logging.getLogger(__name__)
 
 
@@ -51,11 +53,19 @@ class Configurator(ConfiguratorCore):
         setattr(self.control, k, v)
 
 
-def load_config(config, path=None):
+def load_config(config, path=None, strict=False):
     path = path or config.config_path
     if not os.path.exists(path):
         logger.info("%s is not found. create as default configuration")
-        config.init_config(path=path)
+        if strict:
+            raise ConfigNotFound(path)
+        else:
+            msg = """\
+configuration file({}) is not found.
+  plese run 'cctm init' or 'cctm init --project=.'
+""".format(path)
+            sys.stderr.write(msg)
+            sys.exit(-1)
 
     with open(path) as rf:
         config.settings = config.control.settings = settings = json.load(rf)  # xxx
