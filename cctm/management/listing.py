f7db489662e493bb4a9035d90ebaff064ffc612d
podhmo <podhmo+altair@beproud.jp>
2016-01-10 17:05:04 +0900

    alias support..

--- cctm/management/listing.py
+++ cctm/management/listing.py
@@ -1,21 +1,30 @@
 # -*- coding:utf-8 -*-
+import sys
 from cctm import services
 from cctm.path import safe_listdir
 
 
-def main(config, installed=False):
+def main(config, installed=False, alias=False):
     config.load_config()
     if installed:
         for path in safe_listdir(config.template_dir):
             print(path.replace(".", "/", 1))
+    elif alias:
+        store = services.aliases_store(config)
+        for data in store.load():
+            print("{data[name]} -> {data[link]}".format(data=data))
     else:
-        store = services.packages_store(config)
-
         def gen():
+            sys.stderr.write("-- template --\n")
+            store = services.packages_store(config)
             for data in store.load():
                 yield ("{data[name]}({data[star]}) -- {data[description]:.60}".format(data=data))
+            sys.stderr.write("-- alias --\n")
+            store = services.aliases_store(config)
+            for data in store.load():
+                yield("{data[name]} -> {data[link]}".format(data=data))
 
-        for line in sorted(gen()):
+        for line in gen():
             print(line)
 
 
--- cctm/services/lookup.py
+++ cctm/services/lookup.py
@@ -2,7 +2,7 @@
 import os.path
 from cached_property import cached_property as reify
 from cctm.path import safe_listdir
-from . import packages_store
+from . import packages_store, aliases_store
 
 
 def normalize(name):
@@ -12,6 +12,10 @@ def normalize(name):
 class InstalledPackageLookup(object):
     def __init__(self, config):
         self.config = config
+
+    @reify
+    def aliases(self):
+        return aliases_store(self.config).load()
 
     def load(self):
         r = []
@@ -24,10 +28,12 @@ class InstalledPackageLookup(object):
         return r
 
     def lookup_loose(self, name, exists_data=None):
+        name = get_name(name, self.aliases)
         exists_data = exists_data or self.load()
         return lookup_loose(name, exists_data)
 
     def lookup(self, name, exists_data=None):
+        name = get_name(name, self.aliases)
         exists_data = exists_data or self.load()
         return lookup(name, exists_data)
 
@@ -37,19 +43,28 @@ class PackageLookup(object):
         self.config = config
 
     @reify
-    def store(self):
-        return packages_store(self.config)
+    def aliases(self):
+        return aliases_store(self.config).load()
 
     def load(self):
-        return self.store.load()
+        return packages_store(self.config).load()
 
     def lookup_loose(self, name, exists_data=None):
+        name = get_name(name, self.aliases)
         exists_data = exists_data or self.load()
         return lookup_loose(name, exists_data)
 
     def lookup(self, name, exists_data=None):
+        name = get_name(name, self.aliases)
         exists_data = exists_data or self.load()
         return lookup(name, exists_data)
+
+
+def get_name(name_or_alias, exists_data):
+    alias = lookup_loose(name_or_alias, exists_data)
+    if alias is not None:
+        return alias["link"]
+    return name_or_alias
 
 
 def lookup(name, exists_data):
